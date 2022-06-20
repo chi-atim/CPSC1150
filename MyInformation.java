604b930addd06e86f196c1cfd38dec81593212ed
Chi <chiemi2400@gmail.com>
2022-06-20 12:32:15 -0700

    Add files via upload

---
+++ MyInformation.java
@@ -0,0 +1,18 @@
+/**
+*MyInformation App class implements an application that
+*displays "My Informations" to the standard output.
+*/
+public class MyInformation
+{
+   public static void main(String[]args)
+   {
+      System.out.println("************************************************");
+      //Displays "Your name"
+      System.out.println("*  Name: Chiemi Mita                     *");
+      //Displays "Student Number"
+      System.out.println("*  Student Number: 100320919                        *");
+      //Displays "The list of the courses you are taking"
+      System.out.println("*  Courses: CPSC1045, CPSC1050, CPSC1150, MATH1171  *");
+      System.out.println("************************************************");
+   }
+}
