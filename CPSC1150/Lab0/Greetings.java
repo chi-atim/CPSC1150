2377da1683adaa467eb710107eeba38b461912f4
Chi <chiemi2400@gmail.com>
2022-06-21 01:09:00 -0700

    Create Greetings.java

---
+++ CPSC1150/Lab0/Greetings.java
@@ -0,0 +1,18 @@
+/**
+*Greetings App class implements an application that
+*displays "Hello World!" and My Informations to the standard output.
+*/
+
+
+public class MyInformation
+{
+   public static void main(String[]args)
+   {
+      System.out.println("Hello World!");
+      System.out.println("************************************************");
+      System.out.println("*  Name: Chiemi Mita                           *");
+      System.out.println("*  Student Number: 100320919                   *");
+      System.out.println("*  Program: Computer Science                   *");
+      System.out.println("************************************************");
+   }
+}
