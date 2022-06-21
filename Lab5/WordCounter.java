1ab811bdaa26089b186a8a40c29034ce9993327a
Chi <chiemi2400@gmail.com>
2022-06-21 13:00:30 -0700

    Add files via upload

---
+++ Lab5/WordCounter.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class WordCounter
+{
+   public static void main(String[] args)
+	{
+      Scanner input = new Scanner (System.in);
+      String sent;
+      
+      System.out.println("Please enter a sentence: ");
+      sent = input.nextLine();
+      sent = sent.trim();
+      int words = sentCounter(sent);
+      
+      System.out.print("There are " + words + " words in\n" + "\"" + sent + "\"");
+	}
+
+	public static int sentCounter (String str)
+	{
+      int count = 0;
+      
+      for(int i = 0; i < str.length(); i ++)
+      {
+         if ((str.charAt(i) == ' ') && (str.charAt(i+1) != ' '))
+         {
+            count++;
+         }
+      }
+      return count + 1;
+   }
+}
