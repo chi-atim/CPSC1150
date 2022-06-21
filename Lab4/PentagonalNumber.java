5e892200be8fcaba188ef3ac3684e58353ebedef
Chi <chiemi2400@gmail.com>
2022-06-21 12:59:05 -0700

    fixed bugs

--- Lab4/PentagonalNumber.java
+++ Lab4/PentagonalNumber.java
@@ -1,17 +1,16 @@
-public class PentagonalNumber
-{
-	public static void main(String[] args)
-	{
-      for (int n = 1 ; n <= 10 ; n ++ )
-      {
-         System.out.print(getPentagonalNumber(n) + "   ");
+public class PentagonalNumber {
+	public static void main(String[] args) {
+            for (int i = 1 ; i <= 10 ; i++) {
+                  System.out.print(getPentagonalNumber(i));
+                  if (i % 10 == 0) {
+                        System.out.println();
+                  } else {
+                        System.out.print("\t");
+                  }
+	      }
       }
-	}
 
-	public static int getPentagonalNumber(int num)
-	{
-      int result;
-      result = num * (3 * num - 1) / 2;
-      return result;
+      public static int getPentagonalNumber(int num) {
+            return (3 * num * num - num) / 2;
 	}
 }
