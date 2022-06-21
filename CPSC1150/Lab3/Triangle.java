8904c5df2d365930168d267e4ab2d7cf38febd74
Chi <chiemi2400@gmail.com>
2022-06-21 11:36:24 -0700

    add package

--- CPSC1150/Lab3/Triangle.java
+++ CPSC1150/Lab3/Triangle.java
@@ -1,1 +1,35 @@
+package CPSC1150.Lab3;
 
+import java.util.Scanner;
+public class Triangle
+{
+   public static void main(String[] args)
+	{
+		Scanner input = new Scanner (System.in);
+      int num, row, col, space;
+
+		System.out.print("Please enter a number from 1 - 9: ");
+		num = input.nextInt();
+      
+      if (num < 0 || num > 9)
+      {   
+         System.out.println("The number must be 1-9"); 
+      }
+      else
+      {
+		   for (row = 1; row <= num; row ++)
+         {
+            for (space = num - row; space >= 1; space--)
+            {
+               System.out.print(" ");
+            }
+            for (col = 1; col <= row; col++)
+            {
+               System.out.print(col);
+            }
+            System.out.println("");
+         }
+      }
+      input.close();
+   }
+}
