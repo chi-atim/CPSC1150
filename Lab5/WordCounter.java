ede1d5d3f5eed3b3eefe20d81e41fff5581b778f
Chi <chiemi2400@gmail.com>
2022-06-21 13:21:38 -0700

    updated

--- Lab5/WordCounter.java
+++ Lab5/WordCounter.java
@@ -4,19 +4,20 @@ public class WordCounter{
    public static void main(String[] args)	{
       Scanner input = new Scanner (System.in);
       String sent;
-      
+
       System.out.print("Please enter a sentence: ");
       sent = input.nextLine();
       sent = sent.trim();
       int words = sentCounter(sent);
-      
-      System.out.print("There are " + words + " words in\n" + "\"" + sent + "\"");
-      input.close();
-	}
 
-	public static int sentCounter (String str) {
-      int count = 0;
+      System.out.print("There are " + words + " words in\n" + "\"" + sent + "\"");
       
+      input.close();
+   }
+
+   public static int sentCounter (String str) {
+      int count = 0;
+
       for(int i = 0; i < str.length(); i ++) {
          if ((str.charAt(i) == ' ') && (str.charAt(i+1) != ' ')) {
             count++;
--- Lab6/Zellers.java
+++ Lab6/Zellers.java
@@ -1,36 +1,33 @@
 import java.util.Scanner;
 public class Zellers
 {
-   public static void main (String[] args)
-	{
+   public static void main (String[] args) {
 		Scanner input = new Scanner (System.in);
       System.out.println("Please enter a year greater than 1582: ");
       int year = input.nextInt();
 		
       int z = zellers(year);
       System.out.println("The first day of January " + year + " is on " + dayOfWeek(z));
+
+      input.close();
    }
 	
-	public static int zellers (int year)
-	{
-		int DayNumber, StartMonth = 0, StartYear = 0, LeapFactor;
+	public static int zellers (int year) {
+		int StartMonth = 0, StartYear = 0, LeapFactor;
 		int month, day;
       month = 1; // January
       day = 1; // 1st of January
       
-		if (month < 3)
-		{
+		if (month < 3)	{
 			StartMonth = 0;
 			StartYear = year - 1;
 		}
 		LeapFactor = (StartYear/4) - (StartYear/100) + (StartYear/400);
-		return DayNumber = ((365 * year + 31 * (month - 1) + day + LeapFactor - StartMonth)-1) % 7;
+		return ((365 * year + 31 * (month - 1) + day + LeapFactor - StartMonth)-1) % 7;
    }
    
-   public static String dayOfWeek (int z)
-	{
-      switch(z)
-      {
+   public static String dayOfWeek (int z)	{
+      switch(z) {
          case 0: return "Sunday";
          case 1: return "Monday";
          case 2: return "Tuesday";
