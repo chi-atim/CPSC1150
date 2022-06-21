46c4afb5dfaad44f58e9ad24126162c1ad2f4ea9
Chi <chiemi2400@gmail.com>
2022-06-21 13:06:43 -0700

    organized code

--- Lab5/WordCounter.java
+++ Lab5/WordCounter.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
-public class WordCounter
-{
-   public static void main(String[] args)
-	{
+
+public class WordCounter{
+   public static void main(String[] args)	{
       Scanner input = new Scanner (System.in);
       String sent;
       
-      System.out.println("Please enter a sentence: ");
+      System.out.print("Please enter a sentence: ");
       sent = input.nextLine();
       sent = sent.trim();
       int words = sentCounter(sent);
       
       System.out.print("There are " + words + " words in\n" + "\"" + sent + "\"");
+      input.close();
 	}
 
-	public static int sentCounter (String str)
-	{
+	public static int sentCounter (String str) {
       int count = 0;
       
-      for(int i = 0; i < str.length(); i ++)
-      {
-         if ((str.charAt(i) == ' ') && (str.charAt(i+1) != ' '))
-         {
+      for(int i = 0; i < str.length(); i ++) {
+         if ((str.charAt(i) == ' ') && (str.charAt(i+1) != ' ')) {
             count++;
          }
       }
