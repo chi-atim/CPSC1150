c258269007c62b545804bdd66078db5fee458e46
Chi <chiemi2400@gmail.com>
2022-06-21 01:13:16 -0700

    Create Cylinder.java

---
+++ CPSC1150/Lab1/Cylinder.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Cylinder 
+{
+   public static void main(String[] args)
+   {
+      // Create a Scanner object
+      Scanner input = new Scanner(System.in);
+      
+      // Prompt the user to enter a radius and a length
+      System.out.print("Enter the radius of a cylinder: ");
+      double radius = input.nextDouble();
+      System.out.print("Enter the length of a cylinder: ");
+      int length = input.nextInt();
+      
+      // Compute area
+      double area = radius * radius * Math.PI;
+      double volume = area * length;
+      
+      // Display results
+      System.out.println("The volume of a cylinder is " + volume);
+   }
+}
