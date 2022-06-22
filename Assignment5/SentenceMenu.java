72bc1f5209d4cd79eb9b114bf68cd20ea98a4c2a
Chi <chiemi2400@gmail.com>
2022-06-21 22:33:53 -0700

    Update SentenceMenu.java

--- Assignment5/SentenceMenu.java
+++ Assignment5/SentenceMenu.java
@@ -1,24 +1,22 @@
 /*
 **Program Name: SentenceMenu
-**Author: Chiemi Mita
-**Class Section: CPSC1150-004
-**Instructor: Hengameh Hamavand
-**Date: October 14th, 2018
+** This program reads a sentence (one line of text) from the user, it then displays a menu for different tasks.
 */
 
 import java.util.Scanner;
-public class SentenceMenu
-{
-   public static void main(String[] args)
-	{
+
+public class SentenceMenu {
+	public static void main(String[] args) {
 		Scanner input = new Scanner (System.in);
 		String sent;
-		System.out.println("Please enter a sentence: "); // Read the user input sentence 
+
+		// Read the user input sentence
+		System.out.println("Please enter a sentence: "); 
 		sent = input.nextLine();
 		sent = sent.trim(); //Trim the blanks
 		int action;
-		do
-		{
+
+		do {
 			Scanner menu = new Scanner (System.in);
 			System.out.println("\nPlease enter: "
 			+ "\n1. To display the number of words in the sentence."
@@ -28,107 +26,112 @@ public class SentenceMenu
 			+ "\n5. To enter a new statement."
 			+ "\n6. To terminate the program.\n");
 			action = menu.nextInt();
-			switch (action)
-			{
-				case 1:		wordCounter(sent); //Call the wordCounter method
-								break;
-				case 2:		vowelCounter(sent); //Call the vowelCounter method
-								break;
-				case 3:		longestWord(sent); //Call the longestWord method
-								break;
-				case 4:		mostVowel(sent); //Call the mostVowel method
-								break;
-				case 5:		sent = newStatement(sent); //Call the newStatement method
-								break;
-				case 6:		System.out.println("Program terminates.");// Terminates the program
-								break;
-				default:	System.out.println("Error: invalid status"); // Error msg for invalid inputs
+			
+			switch (action) {
+				case 1:	
+					wordCounter(sent);
+					break;
+				case 2:	
+					vowelCounter(sent);
+					break;
+				case 3:	
+					longestWord(sent);
+					break;
+				case 4:	
+					mostVowel(sent);
+					break;
+				case 5:	
+					sent = newStatement(sent);
+					break;
+				case 6:	
+					System.out.println("Program terminates.");
+					break;
+				default:
+					// Error msg for invalid inputs
+					System.out.println("Error: invalid status");
 			}
-		}while (action != 6);
+		} while (action != 6);
+		input.close();
 	}
 
-	public static void wordCounter (String str)  //Case 1: method
-	{
+	//Case 1: wordCounter method
+	public static void wordCounter (String str) {
 		int count = 0;
-		for(int i = 0; i < str.length(); i ++)
-		{
-			if ((str.charAt(i) == ' ') && (str.charAt(i+1) != ' '))
-			{
+		for(int i=0; i<str.length(); i++) {
+			if ((str.charAt(i) == ' ') && (str.charAt(i+1) != ' ')) {
 				count++;
 			}
 		}
 		count ++;
 		System.out.println("\nThere are " + count + " words in " + "\"" + str + "\"");
-    }
+	}
 
-   public static void vowelCounter (String str) //Case 2: method
-	{
+	//Case 2: vowelCounter method
+	public static void vowelCounter (String str) {
 		int aCount = 0, eCount = 0, iCount = 0, oCount = 0, uCount = 0;
 		String s = str.toLowerCase();
-		for(int i = 0; i < s.length(); i ++)
-		{
-			switch(s.charAt(i))
-			{
-				case 'a' :	aCount++;
-								break;
-				case 'e' :	eCount++;
-								break;
-				case 'i' :	iCount++;
-								break;
-				case 'o' :	oCount++;
-								break;
-				case 'u' :	uCount++;
-								break;
-				default:	continue;
+
+		for(int i=0; i < s.length(); i++) {
+			switch(s.charAt(i)) {
+				case 'a':
+					aCount++;
+					break;
+				case 'e':
+					eCount++;
+					break;
+				case 'i':
+					iCount++;
+					break;
+				case 'o':
+					oCount++;
+					break;
+				case 'u':
+					uCount++;
+					break;
+				default:
+					continue;
 			}
 		}
 		System.out.println("\nThere are:\n\t" + aCount + " a's\n\t" + eCount + " e's\n\t" + iCount + " i's\n\t" + oCount + " o's\n\t" + uCount +" u's \n in \"" + str + "\"");
-    }
+	}
    
-	public static void longestWord (String str) //Case 3: method
-	{
+	// Case 3: longestWord method
+	public static void longestWord (String str) {
 		int max = 0;
 		String longest = "", word = "";
-		for(int i = 0; i < str.length(); i ++)
-		{
+		
+		for(int i=0; i<str.length(); i++) {
 			char k = str.charAt(i);
-			if (k != ' ')
-			{
+			if (k != ' ') {
 				word = word + k ; 
 			}
-			if (k == ' ' ||  i == str.length() - 1)
-			{
+			if (k == ' ' ||  i == str.length() - 1) {
 				int n = word.length();
-				if (n > max)
-				{
+				if (n > max) {
 					max = n;
 					longest = word;
 				}
 				word = ""; // reset word
 			}
 		}
-	  System.out.println("\nThe longest word is: " + longest);
-    }
+		System.out.println("\nThe longest word is: " + longest);
+	}
 
-   public static void mostVowel (String str) //Case 4: method
-	{
+	//Case 4: method
+	public static void mostVowel (String str) {
 		String word = "", mostV = "";
 		int max=0, count=0;
-		for(int i = 0; i < str.length(); i ++)
-		{
+		
+		for(int i=0; i<str.length(); i++) {
 			char k = str.charAt(i);
-			if (k != ' ')
-			{
+			if (k != ' ') {
 				word = word + k ;
-				if (k == 'a' || k == 'e' || k == 'i' || k == 'o' || k == 'u' || k == 'A' || k == 'E' || k == 'I' || k == 'O' || k == 'U')
-				{
+				if (k == 'a' || k == 'e' || k == 'i' || k == 'o' || k == 'u' || k == 'A' || k == 'E' || k == 'I' || k == 'O' || k == 'U') {
 					count ++;
 				}
 			}
-			if (k == ' ' && str.charAt(i+1) != ' ' || i == str.length() - 1)
-			{
-				if (count > max)
-				{
+			if (k == ' ' && str.charAt(i+1) != ' ' || i == str.length() - 1) {
+				if (count > max) {
 					mostV = word;
 					max = count;
 				}
@@ -137,9 +140,9 @@ public class SentenceMenu
 		}
 		System.out.println("\nThe word with the most vowels is: " + mostV);
 	}
-	
-   public static String newStatement (String str) //Case 5: method
-	{
+
+	//Case 5: method
+	public static String newStatement (String str) {
 		Scanner input = new Scanner (System.in);
 		String newS;
 		System.out.println("\nPlease enter a new statement: ");
