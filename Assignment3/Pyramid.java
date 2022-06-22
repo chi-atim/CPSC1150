fcfa1c87f42e434ef4fa5ae3d3a22ca14d3c8d0e
Chi <chiemi2400@gmail.com>
2022-06-21 21:26:11 -0700

    Update Pyramid.java

--- Assignment3/Pyramid.java
+++ Assignment3/Pyramid.java
@@ -1,41 +1,33 @@
 /*
 **Program Name: Pyramid
-**Author: Chiemi Mita
-**Class Section: CPSC1150-004
-**Instructor: Hengameh Hamavand
+**This program prompts the user to enter an integer from 1 to 9 and display a pyramid.
 **Date: September 30th, 2018
 */
 
 import java.util.Scanner;
-public class Pyramid
-{
-	public static void main(String[] args)
-	{
+
+public class Pyramid {
+	public static void main(String[] args) {
+		
 		Scanner input = new Scanner (System.in); // Create a Scanner
 		int num, row, col, space; //All to be integer numbers
-		
+
 		// Prompt the user to enter a number
 		System.out.print("Please enter a number from 1 - 9: "); 
 		num = input.nextInt();
-		
-		if (num < 1 || num > 9)
-		{
+
+		if (num < 1 || num > 9) {
 			System.out.println("The number must be 1 - 9"); //Display an error message
 		}
-		else
-		{
-			for (row = 1; row <= num; row ++ )
-			{
-				for (space = num - row; space >= 1; space --)
-				{
+		else {
+			for (row = 1; row <= num; row ++ ) {
+				for (space = num - row; space >= 1; space --) {
 					System.out.print("  "); //Display spaces
 				}
-				for (col = 1; col < row; col ++)
-				{
+				for (col = 1; col < row; col ++) {
 					System.out.print(row-col+1); //Display the left side of the pyramid
 				}
-				for (col = 1; col <= row; col ++)
-				{
+				for (col = 1; col <= row; col ++) {
 					System.out.print(col); //Display the right side of the pyramid
 				}
 				System.out.println("");
